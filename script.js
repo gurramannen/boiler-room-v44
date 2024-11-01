2944b159278bad6e8e1d5107eb5035e25e108dea
rebeccalindman <rebeccalindman@icloud.com>
2024-11-01 10:47:08 +0100

    strucure in place

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// Global variables array **** kanske överflödigt, kommer från chatGPT-workflow
+//const globalVariables = [];
+
+// Task array
+let listOfTasks = [];
+console.log(listOfTasks);
+
+
+//Object for tasks -- default structure
+
+let task = {
+    taskId:  1,
+    title: "title",
+    description: "description",
+    isComplete: false
+   };
+console.log(task);
+
+
+// do while loop menu
+
+
+
+// Function to add tasks
+
+
+// Function to complete tasks 
+
+
+// Function to remove tasks
+
+
+// Function to show all tasks
