d84ad1733281b452eb333be29a9200ac2a68e3d4
rebeccalindman <rebeccalindman@icloud.com>
2024-11-08 10:14:52 +0100

    basic structure started

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// Add task 
+
+// event listener for add new task button
+const newTaskForm = document.getElementById("newTaskForm");
+const addTaskButton = document.getElementById("addTaskButton");
+addTaskButton.addEventListener("click", function(event) {
+    event.preventDefault;
+    addNewTask();
+
+});
+
+
+// add and append new tasks
+function addNewTask(){
+    const taskList = document.getElementById("activeTasks");
+    let newTask = document.createElement("li");
+
+    //TODO checkbox mark as complete
+
+    //TODO button to remove
+
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "x";
+    removeButton.addEventListener("click", function () {
+        
+        newTask.remove();
+        //TODO if it´s empty
+
+    });
+    
+    //TODO text for new task
+    newTask.innerText = document.getElementById("newTaskInput").value;
+    
+    
+};
+
+taskList.appendChild(newTask); //append li to taskList ul
+newTask.appendChild(removeButton);
+
+// Mark task as complete
+
+
+
+// Remove task
+
+
