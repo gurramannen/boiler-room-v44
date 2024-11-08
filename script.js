2cb290ecd50d7ec1aec72383d9423354b844fc86
rebeccalindman <rebeccalindman@icloud.com>
2024-11-08 15:55:58 +0100

    task decription in place, non-obligatory to fill in

--- script.js
+++ script.js
@@ -5,15 +5,16 @@
 const newTaskForm = document.getElementById("newTaskForm");
 const addTaskButton = document.getElementById("addTaskButton");
 const newTaskInput = document.getElementById("newTaskInput");
+const taskDescription = document.getElementById("newTaskDescription");
 const taskList = document.getElementById("activeTasks");
 // const felmeddelande = document.getElementById("error-message"); //old error message
 const errorMessage = document.getElementById("errorMessage");
-errorMessage.style.display = "none";
+errorMessage.style.display = "none"; // initially hides the error message
 // event listener for add new task button
 
 addTaskButton.addEventListener("click", function(event) {
     event.preventDefault();
-    addNewTask(newTaskInput.value);
+    addNewTask(newTaskInput.value, taskDescription.value);
     newTaskInput.value = ""; // clear input field after adding task
 
 });
@@ -21,7 +22,7 @@ addTaskButton.addEventListener("click", function(event) {
 // add and append new tasks
 
 
-function addNewTask(taskText){
+function addNewTask(taskText, taskDescription){
     
     if (newTaskInput.value.trim() === "") {
          errorMessage.style.display = "block";
@@ -30,10 +31,10 @@ function addNewTask(taskText){
         
     } else {
     errorMessage.style.display = "none";
-    
+
     let newTask = document.createElement("li");
 
-    //TODO checkbox mark as complete
+    // mark as complete
     let isComplete = false;
     function completeTask(isComplete) {
     if (isComplete) {
@@ -56,16 +57,26 @@ function addNewTask(taskText){
     newTask.appendChild(markButton);
 
     //text for new task
-   
-
     let textNode = document.createTextNode(" " + taskText);
     newTask.appendChild(textNode);
+
+    // description of task
+    let description = document.createElement("p");
+    if (taskDescription === "") {
+        description.innerText = "";
+    } else {
+        description.innerText = "Beskrivning: " + taskDescription;
+        newTaskDescription.value = ""; // clear input field after adding task
+    }
+    newTask.appendChild(description);
 
     //button to remove
     let removeButton = document.createElement("button");
     removeButton.innerText = "x";
     removeButton.addEventListener("click", function () {
         newTask.remove();
+
+    
 
     });
     newTask.appendChild(removeButton);
